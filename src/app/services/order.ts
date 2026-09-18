d07db169204d6a667a1f6295116c3ea021ae9a3f
Basmallah Hossam <basmallahhossam36@gmail.com>
2026-09-19 00:04:33 +0300

    Update backend URLs

--- src/app/services/order.ts
+++ src/app/services/order.ts
@@ -46,8 +46,7 @@ export class OrderService {
 
   private http = inject(HttpClient);
 
-  private apiUrl = 'https://elghandourabackend1-f06md7r7.b4a.run/orders';
-
+private apiUrl = 'https://elghandourabackend1-w1k7uvlt.b4a.run/orders';
   createOrder(
     data: CreateOrderData
   ): Observable<CreateOrderResponse> {
